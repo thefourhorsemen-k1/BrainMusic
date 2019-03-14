f209cf5583495b4f86410cf58606243d3766a364
thefourhorsemen-4 <fourhorsemen987@gmail.com>
2019-03-14 11:42:22 +0700

    change to deploy

--- src/main/java/com/horsemen/bmserver/security/jwt/JwtProvider.java
+++ src/main/java/com/horsemen/bmserver/security/jwt/JwtProvider.java
@@ -6,9 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
